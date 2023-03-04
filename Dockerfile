e71ef2523a71743b619748e6d3eb9b74c767bbc4
FZapata <97852383+felipezapata01@users.noreply.github.com>
2023-03-04 17:22:53 -0500

    Configure postgres db with temporary credentials and set python3 version in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3
+FROM python:3.8.10
 
 # The enviroment variable ensures that the python output is set straight
 # to the terminal with out buffering it first
--- urlshortener/urlshortener/settings.py
+++ urlshortener/urlshortener/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -66,8 +66,12 @@ WSGI_APPLICATION = 'urlshortener.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'pos1234',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
